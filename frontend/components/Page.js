2c0e3b8bd93acfbf3d87c57e828686a3d366469a
Alyson Takai <shtakai@gmail.com>
2018-10-22 21:00:02 +0700

    f]Apply styled component

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,6 +1,15 @@
 import React, {Component} from 'react';
+import styled from 'styled-components'
 import Header from './Header'
 import Meta from './Meta'
+
+const SuckButton = styled.button`
+  background: red;
+  font-size: ${props => (props.huge ? '75px' : '40px')};
+  span.poop {
+    font-size: 100px;
+  }
+`
 
 class Page extends Component {
   render() {
@@ -8,6 +17,14 @@ class Page extends Component {
       <div>
         <Meta />
         <Header />
+        <SuckButton huge>
+          Fuck Me I'm suck\
+          <span className={`poop`}>💩</span>
+        </SuckButton>
+        <SuckButton>
+          Fuck Me I'm fucked\
+          <span className={`poop`}>🐔</span>
+        </SuckButton>
         {this.props.children}
       </div>
     );
