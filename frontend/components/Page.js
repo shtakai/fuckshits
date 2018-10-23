24ce5af4e4f8d5cc5d9dae15927bed2a2889716e
Alyson Takai <shtakai@gmail.com>
2018-10-23 15:27:08 +0700

    f]Apply styled components on Page

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,15 +1,39 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import styled, { ThemeProvider, injectGlobal} from 'styled-components'
+
 import Header from './Header'
 import Meta from './Meta'
+
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  lightGrey: '#3A3A3A',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.9)',
+}
+
+const StyledPage = styled.div`
+  background: ${props => props.theme.white};
+  color: ${props => props.theme.black};
+`
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`
 
 class Page extends Component {
   render() {
     return (
-      <div>
-        <Meta />
-        <Header />
-        {this.props.children}
-      </div>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
