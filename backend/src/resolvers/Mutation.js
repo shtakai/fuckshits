104eaef23353ae3a59eecc134713ec245da8f3a5
Alyson Takai <shtakai@gmail.com>
2018-11-01 20:44:53 +0700

    b]Implement simple mutation/query
    - japs
    - createJap

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const Mutations = {}
+const Mutations = {
+  createJap(parent, args, ctx, info) {
+    global.japs = global.japs || []
+    const newJap = { name: args.name }
+    global.japs.push(newJap)
+    return newJap
+  }
+
+}
 
 module.exports = Mutations
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,9 @@
-const Query = {}
+const Query = {
+  japs(parent, args, ctx, info) {
+    global.japs = global.japs || []
+    return global.japs
+  }
+
+}
 
 module.exports = Query
