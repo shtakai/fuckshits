63bbe2f7d2319314e189547afcd83615eca7b966
Alyson Takai <shtakai@gmail.com>
2018-11-01 22:01:20 +0700

    b]Modify mutation/query for using schema properly

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,11 +1,14 @@
 const Mutations = {
-  createJap(parent, args, ctx, info) {
-    global.japs = global.japs || []
-    const newJap = { name: args.name }
-    global.japs.push(newJap)
-    return newJap
-  }
+  async createItem(parent, args, ctx, info) {
+    // TODO: check if they are logged in
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info)
 
+    return item
+  }
 }
 
 module.exports = Mutations
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,8 +1,11 @@
+const { forwardTo } = require('prisma-binding')
+
 const Query = {
-  japs(parent, args, ctx, info) {
-    global.japs = global.japs || []
-    return global.japs
-  }
+  items: forwardTo('db'),
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items()
+  //   return items
+  // }
 
 }
 
