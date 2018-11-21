1285fbd3f5982e6b9dae1631b4e14d2c52f2d26e
Alyson Takai <shtakai@users.noreply.github.com>
2018-11-21 19:50:31 +0700

    Feature/prisma (#4)
    * b]Generate graphql file
    
    * b]Implement simple server
    
    * b]Implement simple mutation/query
    
    - japs
    - createJap
    
    * b]Modify schema
    
    * b]Modify mutation/query for using schema properly

---
+++ backend/src/resolvers/Mutation.js
@@ -0,0 +1,14 @@
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: check if they are logged in
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info)
+
+    return item
+  }
+}
+
+module.exports = Mutations
